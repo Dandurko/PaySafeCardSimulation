e1f762309d649ddb1209348f9898ad0e410efdb5
dandur <deniscicoo@gmail.com>
2024-07-03 16:48:17 +0200

    Initial Commit

---
+++ src/test/java/com/paysafe/Paysafe/PaysafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paysafe.Paysafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaysafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
