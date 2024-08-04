84fb5a0bb395028d57392f6e10c322c134669ae6
dandur <deniscicoo@gmail.com>
2024-08-04 14:12:36 +0200

    Generate JWT

---
+++ src/test/java/com/paysafe/Paysafe/JwtSecretMakerTest.java
@@ -0,0 +1,18 @@
+package com.paysafe.Paysafe;
+
+import io.jsonwebtoken.Jwts;
+import jakarta.xml.bind.DatatypeConverter;
+import org.junit.jupiter.api.Test;
+
+import javax.crypto.SecretKey;
+
+public class JwtSecretMaker {
+    @Test
+    public void generateSecretKey() {
+        SecretKey key = Jwts.SIG.HS512.key().build();
+        String encodedKey = DatatypeConverter.printHexBinary(key.getEncoded());
+
+        System.out.println("Encoded key = " + encodedKey);
+
+    }
+}
